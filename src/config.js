ede538cd872767b797baef37694a4572e60df36e
Nghi Tran <nghi.t.tran8@gmail.com>
2020-03-13 14:45:53 -0700

    update styling

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: "https://petful-server-nhgi-taylorp.herokuapp.com/api",
-  TOKEN_KEY: "settingupnow"
+  REACT_APP_API_BASE:
+    process.env.REACT_APP_API_BASE ||
+    "https://petful-server-nhgi-taylorp.herokuapp.com/api"
 };
